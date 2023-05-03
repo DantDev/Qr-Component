7b1bd3fa3ea6f81fa4c9dcafa1505f1b6335278d
DantDev <118768256+DantDev@users.noreply.github.com>
2023-05-03 20:42:24 +0100

    Initial Deploy

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import qrCode from "./assets/image-qr-code.png"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <div className='card'>
+        <img src={qrCode} />
+        <h2 className='card--title' >Improve your front-end skills by building projects</h2>
+        <p className='card--text'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
